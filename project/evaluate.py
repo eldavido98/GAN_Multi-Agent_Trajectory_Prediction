c26a450596033808ce5b0c8cb0c8880290593c58
eldavido98 <146634840+eldavido98@users.noreply.github.com>
2025-11-29 12:05:02 +0100

    Update evaluate.py

--- project/evaluate.py
+++ project/evaluate.py
@@ -8,8 +8,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_set = upload_dataset("test_set")
 
 # flag = [scene, traffic_light, state] (0: yes, 1: no)
-# forecaster = Forecasting(concat_dim=1280, flag=[0, 0, 0], device=device).to(device)
-forecaster = Forecasting(concat_dim=1216, flag=[1, 0, 0], device=device).to(device)
+forecaster = Forecasting(concat_dim=1280, flag=[0, 0, 0], device=device).to(device)
+# forecaster = Forecasting(concat_dim=1216, flag=[1, 0, 0], device=device).to(device)
 # forecaster = Forecasting(concat_dim=1152, flag=[1, 1, 0], device=device).to(device)
 # forecaster = Forecasting(concat_dim=1152, flag=[1, 0, 1], device=device).to(device)
 # forecaster = Forecasting(concat_dim=1088, flag=[1, 1, 1], device=device).to(device)
@@ -17,3 +17,4 @@ forecaster = Forecasting(concat_dim=1216, flag=[1, 0, 0], device=device).to(devi
 forecaster.load(name='your_model.pt')
 
 forecaster.evaluation(test_set=test_set)
+
